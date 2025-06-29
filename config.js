d85015e0a89168ac7a671be3a345a993f613ccd3
VexxuzzZ <rizkybayuaji267@gmail.com>
2025-06-29 12:19:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7893093376:AAG4XdCtm0i633B9QQdBSax8sLNTPGyAp4E",
+  BOT_TOKEN: "7885152417:AAFu-6M7nTNE3VCg7caIfofCxLk4hiwAlLY",
   OWNER_ID: ["7807425271"],
 };
 //@snitchezs
